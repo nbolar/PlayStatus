01d67f4635c7175c9443542180bdf12a6091a197
Nikhil Bolar <bolar.nikhil@gmail.com>
2019-04-25 10:35:21 +0300

    Initial Commit

---
+++ PlayStatus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlayStatus
+//
+//  Created by Nikhil Bolar on 4/25/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
